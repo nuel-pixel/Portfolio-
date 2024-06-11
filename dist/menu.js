a440cc99c4f48f17061c5c7860a010c730baf387
Papa-Kwame Ofosu <pkofosu@persol.com>
2024-06-11 13:10:53 +0000

    first commit

---
+++ dist/menu.js
@@ -0,0 +1,27 @@
+// Getting hamburguer menu in small screens
+const menu = document.getElementById("menu");
+const ulMenu = document.getElementById("ulMenu");
+
+function menuToggle() {
+  menu.classList.toggle("h-32");
+}
+
+function handleMailSubmit() {
+  const subject = document.getElementById("subject").value;
+  const body = document.getElementById("message").value;
+  window.open(
+    `mailto:neighborhoodpixel782@gmail.com?subject=${subject}&body=${body}`
+  );
+}
+
+// Browser resize listener
+window.addEventListener("resize", menuResize);
+
+// Resize menu if user changing the width with responsive menu opened
+function menuResize() {
+  // First get the size from the window
+  const window_size = window.innerWidth || document.body.clientWidth;
+  if (window_size > 640) {
+    menu.classList.remove("h-32");
+  }
+}
